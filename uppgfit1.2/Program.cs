33cbc4a759757197e59529ddcba4685960462a61
DommSite <johan.johansson06@live.se>
2023-09-06 10:40:34 +0200

    Add project files.

---
+++ uppgfit1.2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace errormedelande
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Din dator håller på att dö på grund av din historik. " +
+                "Försök att bli bättre för att bli av med meddelandet");
+
+            
+        }
+    }
+}
